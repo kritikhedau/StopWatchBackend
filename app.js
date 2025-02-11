98c225f792d092b830dc23aa3cf13957ae0755a4
Kritik hedau <kritikhedau@gmail.com>
2025-02-11 23:05:18 +0530

    data base is not getting created at local

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express = require("express");
+const { default: mongoose } = require("mongoose");
 
 const app = express();
 const PORT = 3000;
+const userRoute = require("./routes/user");
 
-app.get('/',(req,res)=>{
+// mongoDb connection
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/stopWatch")
+  .then(() => console.log("MongoDb Connected"))
+  .catch((error) => console.log("Mongo Error", error));
+
+app.get("/", (req, res) => {
   res.status(200);
-  res.send("welcome to myssss serversssssssss")
-})
+  res.send("welcome to myssss serversssssssss");
+});
+
+app.use("/user", userRoute);
 
 app.listen(PORT, (error) => {
   if (!error)
---
+++ controller/user.js
@@ -0,0 +1,12 @@
+async function handleUserSignup(req, res) {
+  const { name, email, password } = req.body;
+  await User.create({
+    name,
+    email,
+    password,
+  });
+  return res.redner("home");
+}
+module.exports = {
+  handleUserSignup
+}
