3b93a7ecbf65161a2b39c13e53509aa7bce5c8f7
Kritik hedau <kritikhedau@gmail.com>
2025-02-12 00:41:28 +0530

    data base setup done

--- controller/user.js
+++ controller/user.js
@@ -1,12 +1,20 @@
+const User = require("../models/user-model");
+
 async function handleUserSignup(req, res) {
-  const { name, email, password } = req.body;
-  await User.create({
-    name,
-    email,
-    password,
-  });
-  return res.redner("home");
+  try {
+    const { name, email, password } = req.body;
+
+    await User.create({
+      name,
+      email,
+      password,
+    });
+
+    return res.send("Signup successful!");
+  } catch (error) {
+    console.error("Signup Error:", error);
+    return res.status(500).send("Something went wrong!");
+  }
 }
-module.exports = {
-  handleUserSignup
-}
+
+module.exports = { handleUserSignup };
