b54c41d3d5b4c1c027f343b3b309bdbbbca1bb22
Kritik hedau <kritikhedau@gmail.com>
2025-02-16 01:21:50 +0530

    Jwt added

--- app.js
+++ app.js
@@ -7,11 +7,12 @@ const userRoute = require("./routes/user");
 // mongoDb connection
 
 const db = require("./config/mongo-connection.js");
+const authenticateToken = require("./middleware/auth.js");
 
 // Middleware to parse JSON
 app.use(express.json()); // This is required for req.body to work
 
-app.get("/", (req, res) => {
+app.get("/", authenticateToken, (req, res) => {
   res.status(200);
   res.send("welcome to lucky");
 });
@@ -25,4 +26,3 @@ app.listen(PORT, (error) => {
     );
   else console.log("Error occurred, server can't start", error);
 });
-
--- controller/user.js
+++ controller/user.js
@@ -1,4 +1,6 @@
 const User = require("../models/user-model");
+const bcrypt = require("bcrypt"); // Import bcrypt
+const jwt = require("jsonwebtoken");
 
 async function handleUserSignup(req, res) {
   try {
@@ -13,7 +15,9 @@ async function handleUserSignup(req, res) {
       });
     }
 
-    await User.create({ name, email, password });
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await User.create({ name, email, password: hashedPassword });
 
     return res.status(201).json({
       status: "success",
@@ -34,13 +38,26 @@ async function handleUserLogin(req, res) {
     const { email, password } = req.body;
 
     // Find user by email and password (plaintext matching)
-    const user = await User.findOne({ email, password });
+    const user = await User.findOne({ email});
 
     if (!user) {
+      return res.status(400).json({ error: "Invalid email" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
       return res.status(400).json({ error: "Invalid username or password" });
     }
 
-    return res.status(200).json({ message: "Login successful" });
+    const token = jwt.sign(
+      { userId: user._id, email: user.email }, // Payload (Data inside token)
+      process.env.JWT_SECRET, // Secret Key
+      { expiresIn: process.env.JWT_EXPIRES_IN } // Expiry Time
+    );
+
+    return res
+      .status(200)
+      .json({ message: "Login successful", status: 1, email: email, token });
   } catch (error) {
     console.error("Login Error:", error);
     return res.status(500).json({
