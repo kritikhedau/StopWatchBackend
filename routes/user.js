968d6386e05b95053b3cdf8211f34b8c64953cd2
Kritik hedau <kritikhedau@gmail.com>
2025-02-16 16:39:44 +0530

    Jwt added

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,14 @@
 const express = require("express");
-const { handleUserSignup, handleUserLogin } = require("../controller/user");
+const { handleUserSignup, handleUserLogin} = require("../controller/user");
 const authenticateToken = require("../middleware/auth");
 
 const router = express.Router();
-router.post("/", handleUserSignup);
+router.post("/Signup", handleUserSignup);
 router.post("/login", handleUserLogin);
-
 router.get("/profile", authenticateToken, (req, res) => {
   res.json({ message: "Welcome to your profile", user: req.user });
 });
+// router.put("/update", authenticateToken, handleUserUpdate); // 🔥 Update User API (Protected)
+
 
 module.exports = router;
